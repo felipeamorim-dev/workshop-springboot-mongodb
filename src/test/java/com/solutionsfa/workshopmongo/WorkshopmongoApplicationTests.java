1e23586b9de90f4df67685f49c94bb3240656732
Felipe Amorim <felipeamorim.dev@gmail.com>
2021-10-27 20:54:21 -0300

    Primeiro commit - Projeto Criado

---
+++ src/test/java/com/solutionsfa/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutionsfa.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
