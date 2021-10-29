35657113a7de25a9be30533cfd8fe4f4b78494b6
Felipe Amorim <felipeamorim.dev@gmail.com>
2021-10-29 20:04:01 -0300

    Adicionando novos usuários com o método POST

--- src/main/java/com/solutionsfa/workshopmongo/service/UserService.java
+++ src/main/java/com/solutionsfa/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.solutionsfa.workshopmongo.domain.User;
+import com.solutionsfa.workshopmongo.dto.UserDTO;
 import com.solutionsfa.workshopmongo.repository.UserRepository;
 import com.solutionsfa.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
