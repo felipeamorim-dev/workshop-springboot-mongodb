bc067c7213666a1623ca10b3ca00edaf18ece43e
Felipe Amorim <felipeamorim.dev@gmail.com>
2021-10-30 00:27:19 -0300

    Consulta com vários critérios

--- src/main/java/com/solutionsfa/workshopmongo/resources/PostResource.java
+++ src/main/java/com/solutionsfa/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.solutionsfa.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,5 +38,16 @@ public class PostResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value="/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate){
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date(0L));
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
 }
 
--- src/main/java/com/solutionsfa/workshopmongo/service/PostService.java
+++ src/main/java/com/solutionsfa/workshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.solutionsfa.workshopmongo.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,5 +25,9 @@ public class PostService {
 	public List<Post> findByTitle(String text){
 		return repository.searchTitle(text);
 	}
-
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24*60*60*1000);
+		return repository.fullSearch(text, minDate, maxDate);
+	}
 }
